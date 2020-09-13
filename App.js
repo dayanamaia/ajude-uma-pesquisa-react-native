c1a4a154ba5f699ad625a73b5c677828e2ddc72b
Dayana Maia <dayrmaia@gmail.com>
2020-09-13 12:24:57 -0300

    feat: adicionado fonts

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading  } from 'expo';
+import { useFonts } from 'expo-font';
+import { Catamaran_400Regular , Catamaran_500Medium } from '@expo-google-fonts/catamaran';
+import { Montserrat_700Bold, Montserrat_800ExtraBold } from '@expo-google-fonts/montserrat';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Catamaran_400Regular,
+    Catamaran_500Medium,
+    Montserrat_700Bold,
+    Montserrat_800ExtraBold,
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <StatusBar style="auto" />
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
